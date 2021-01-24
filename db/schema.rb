4719102e296ffdbeb4676c3b7983ee7ccccb0dd9
gmsamuel <gmsamuel18@gmail.com>
2021-01-24 13:00:35 -0600

    added validations for username

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_182703) do
+ActiveRecord::Schema.define(version: 2021_01_24_185628) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
